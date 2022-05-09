79f32f535cfe3001daa0c1c62bd9a31e0fed75bd
zdw <zdw@qq.com>
2022-05-09 14:17:00 +0800

    push commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master git");
         System.out.println("hot-fix git");
+        System.out.println("push git");
     }
 }
