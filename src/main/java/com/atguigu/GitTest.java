04736b35223064979e804770164f1374e201c61f
zdw <zdw@qq.com>
2022-05-09 10:25:52 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hot-fix git");
     }
 }
